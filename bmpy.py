4aabecdcc70bcb84104af075b96965a8965d2190
j0hn <j0hn.com.ar@gmail.com>
2011-01-26 22:16:11 -0300

    Added fuzzy blur

--- bmpy.py
+++ bmpy.py
@@ -5,7 +5,7 @@ import sys
 import struct
 import StringIO
 from math import ceil
-from random import randint
+import random
 
 class BMPy:
     def __init__(self, filename):
@@ -163,29 +163,36 @@ class BMPy:
 
         self.blur_box(1, 1)
 
-    def box_blur(self, box_width, box_height):
+    def box_blur(self, box_width, box_height, fuzzy=False):
         '''More advance blur where you can specify a box
-        where the blur takes information from'''
+        where the blur takes information from
+        
+        fuzzy effect makes a noizy blur distort'''
 
         copy = self.bitmap[::]
 
         for y in xrange(0, self.height):
             for x in xrange(0, self.width):
-                average = self._blur_box_average(x, y, box_width, box_height, copy)
+                average = self._blur_box_average(x, y, box_width,
+                                                 box_height, copy, fuzzy)
                 self.bitmap[y][x] = average
 
-    def _blur_box_average(self, x, y, bw, bh, copy):
+    def _blur_box_average(self, x, y, bw, bh, copy, fuzzy=False):
         blimit1 = (max(0, x-bw), max(0, y-bh))
         blimit2 = (min(self.width-1, x+bw), min(self.height-1, y+bh))
 
-        total = (0, 0, 0)
-        for y in xrange(blimit1[1], blimit2[1]+1):
-            for x in xrange(blimit1[0], blimit2[0]+1):
-                r, g, b = copy[y][x]
-                total = total[0]+r, total[1]+g, total[2]+b
+        if fuzzy:
+            total = self.bitmap[random.randint(blimit1[1],
+                blimit2[1])][random.randint(blimit1[0], blimit2[0])]
+        else:
+            total = (0, 0, 0)
+            for y in xrange(blimit1[1], blimit2[1]+1):
+                for x in xrange(blimit1[0], blimit2[0]+1):
+                    r, g, b = copy[y][x]
+                    total = total[0]+r, total[1]+g, total[2]+b
 
-        area = (blimit2[0]-blimit1[0]+1)*(blimit2[1]-blimit1[1]+1)
-        total = total[0]/area, total[1]/area, total[2]/area
+            area = (blimit2[0]-blimit1[0]+1)*(blimit2[1]-blimit1[1]+1)
+            total = total[0]/area, total[1]/area, total[2]/area
 
         return total
 
@@ -295,6 +302,7 @@ if __name__ == "__main__":
             bmp.bitmap[y][x] = int(r*f), int(g*t), int(b*p)
     """
 
-    bmp.magic_wand(50, 130, (150, 20, 170), 10)
+    #bmp.magic_wand(50, 130, (150, 20, 170), 10)
+    bmp.box_blur(10, 10, True)
     bmp.save_to("test.bmp")
 
